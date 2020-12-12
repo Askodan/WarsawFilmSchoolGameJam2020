f766fcbd08c60c86af3a99638520ef740d32bf34
Marcin <marcin.karczewski67@gmail.com>
2020-12-12 11:08:22 +0100

    możliwość ustawienia osi obrotu

--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerRotation : MonoBehaviour
 {
+    public Vector3 rotationAxis = Vector3.forward;
     public float dumpThreshold = 0.1f;
     public float accelerationPower = 100;
     public float dumpPower = 50;
@@ -32,7 +33,7 @@ public class PlayerRotation : MonoBehaviour
     private void UpdateRotation()
     {
         currentRotation += rotationSpeed * Time.deltaTime;
-        transform.rotation = Quaternion.Euler(0f, 0f, currentRotation);
+        transform.rotation = Quaternion.Euler(rotationAxis * currentRotation);
     }
     private void OnRotation(InputValue value)
     {
