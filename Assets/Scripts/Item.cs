447db675e2cc04117a3806ff1116d700d2ed7cd2
Marek Kos <kos.marek92@gmail.com>
2020-12-12 19:07:14 +0100

    Items can be picked now

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+  void OnTriggerEnter(Collider other)
+  {
+    Debug.Log("Collision");
+    if(other.gameObject.tag == "Player")
+    {
+      Player p = other.gameObject.GetComponentInParent(typeof(Player)) as Player;
+      p.affect();
+      Destroy(gameObject);
+    }
+  }
+}
