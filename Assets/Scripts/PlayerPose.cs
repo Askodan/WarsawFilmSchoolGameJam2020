a115031f016b9846de4b4b0602cddd25ffe1804d
Marcin <marcin.karczewski67@gmail.com>
2020-12-12 03:15:23 +0100

    inputy wstępnie zrobione

---
+++ Assets/Scripts/PlayerPose.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerPose : MonoBehaviour
+{
+    public Pose[] availablePoses;
+    private int currentPose;
+
+    public int CurrentPose { get => currentPose; }
+
+    void Start()
+    {
+        for (int i = 0; i < availablePoses.Length; i++)
+        {
+            if (currentPose == i)
+                availablePoses[i].Show();
+            else
+                availablePoses[i].Hide();
+        }
+    }
+
+    private void OnChangePose(InputValue value)
+    {
+        float steering = value.Get<float>();
+        int change = (int)Mathf.Sign(steering);
+        ChangeCurrentPose(change);
+    }
+    private void ChangeCurrentPose(int change)
+    {
+        availablePoses[currentPose].Hide();
+        currentPose = (int)Mathf.Repeat(currentPose + change, availablePoses.Length);
+        availablePoses[currentPose].Show();
+    }
+}
---
+++ Assets/Scripts/PoseShower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerPose))]
+public class PoseShower : MonoBehaviour
+{
+    PlayerPose playerPose;
+    void Awake()
+    {
+        playerPose = GetComponent<PlayerPose>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
