f15743217262c24357efced1a5646a27708c6fa6
Marcin <marcin.karczewski67@gmail.com>
2020-12-13 09:23:54 +0100

    minor changes

--- Assets/Scripts/EnergyBarController.cs
+++ Assets/Scripts/EnergyBarController.cs
@@ -13,17 +13,18 @@ public class EnergyBarController : MonoBehaviour
     private Vector3 scaleChange;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         energyBarTransform = gameObject.GetComponent<RectTransform>();
     }
 
     public void modifyEnergy(float modifier)
     {
-      if(energyBarTransform != null) {
+        if (energyBarTransform != null)
+        {
             energy = modifier;
-            scaleChange = new Vector3(1.0f,energy,1.0f);
+            scaleChange = new Vector3(1.0f, energy, 1.0f);
             energyBarTransform.localScale = scaleChange;
-      }
+        }
     }
 }
